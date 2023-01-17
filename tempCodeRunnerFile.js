42ab61d1593bd58da750944eed07abd94adad3b6
Ayushi Pandey <40225444+ayushipandey01@users.noreply.github.com>
2023-01-17 20:09:21 +0530

    vowels removed

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+.includes(vowels)){
+//     for(let i=0;i<str.length;i++){
+//         strFinal+=str[i];
+        
+//     }    
+// }
+
+// console.log(strFinal);
--- vowels_remove.js
+++ vowels_remove.js
@@ -1,3 +1,32 @@
 let str = "The quick brown fox jumps over the lazy dog.";
 
-let vowels = "aeiou";
+let vowels = ["a","e","i","o","u"];
+
+let strFinal = "";
+
+for(let char of str ){
+    if(!vowels.includes(char)){
+        strFinal+=char;
+    }
+}
+
+console.log(strFinal);
+
+
+
+
+// let vowels = "aeiou";
+
+// console.log(str.includes(vowels));
+
+// for(let i=0;i<vowels.length;i++){
+//     // let strFinal = "";
+//     for(let j=0;j<str.length;j++){
+//         if(vowels[i] !== str[j]){
+//             strFinal+=str[j];
+//         }
+//     } 
+//     str = strFinal;   
+// }    
+
+// console.log(strFinal);
