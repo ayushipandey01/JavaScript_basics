d83c5489cfe9065d6e4a7696aeb072401ade4ec1
Ayushi Pandey <40225444+ayushipandey01@users.noreply.github.com>
2023-01-17 17:54:40 +0530

    palindrome_done

--- palindrome_string.js
+++ palindrome_string.js
@@ -1,1 +1,12 @@
-str = "Ayushi";
+let str = "madam";
+let arr = str.split("");
+
+arr.reverse();
+
+let finalOutput = arr.join("");
+
+if(str === finalOutput){
+    console.log("Its a Palindrome!!!");
+}else{
+    console.log("Its not a Palindrome!!!")
+}
